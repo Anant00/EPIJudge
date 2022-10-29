c6cfc5ff6b0c32fdbbf47a645511a282bbf5a292
Anant Pundir <anantpundir02@gmail.com>
2022-10-28 19:55:55 -0500

    Add even_odd code

--- epi_judge_python/even_odd_array.py
+++ epi_judge_python/even_odd_array.py
@@ -8,7 +8,15 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def even_odd(A: List[int]) -> None:
-    # TODO - you fill in here.
+    start = 0
+    end = len(A) - 1
+
+    while start < end:
+        if A[start] % 2 == 0:
+            start += 1
+        else:
+            A[start], A[end] = A[end], A[start]
+            end -= 1
     return
 
 
