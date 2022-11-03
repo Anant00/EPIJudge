665fb890696fdec0ccf83cf4f94b43979c1dc1ad
Anant Pundir <anantpundir02@gmail.com>
2022-11-03 00:49:36 -0500

    Add dutch

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -1,4 +1,7 @@
 import functools
+from operator import eq
+from re import S
+from turtle import st
 from typing import List
 
 from test_framework import generic_test
@@ -9,9 +12,45 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
+    # start = 0
+    # end = len(A) - 1
+    element = A[pivot_index]
+    size = len(A)
+    # for i in range(size):
+    #     if A[i] < element:
+    #         A[i], A[start] = A[start], A[i]
+    #         start += 1
+    
+
+    # for i in reversed(range(size)):
+    #     if A[i] < element:
+    #         break
+            
+    #     if A[i] > element:
+    #         A[i], A[end] = A[end], A[i]
+    #         end -= 1
+
+    
+    equal = 0
+    start = 0
+    larger = size
+    while equal < larger:
+        if A[equal] < element:
+            A[start], A[equal] = A[equal], A[start]
+            start += 1
+            equal += 1
+        elif A[equal] == element:
+            equal += 1
+        else:
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
+
+
+
     return
 
+A = [10, 2, 6, 1, 5, 3, 4, 5, 1, 2, 3, 100, -1]
+dutch_flag_partition(7, A)
 
 @enable_executor_hook
 def dutch_flag_partition_wrapper(executor, A, pivot_idx):
